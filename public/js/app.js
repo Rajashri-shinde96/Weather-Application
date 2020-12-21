635c040922804939313f9e50ef6b28d30a7aea6b
Rajashri Shinde <rshinde@ccrn.com>
2020-12-21 09:54:15 +0530

    first

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                messageTwo.textContent = data.address
             }
         })
     })
